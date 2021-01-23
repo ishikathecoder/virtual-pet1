7e6502a8a5020b343f0ed096c3e432c9a142c744
ishu-theCoder <75232296+ishikathecoder@users.noreply.github.com>
2021-01-23 15:22:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+var dog, happyDog, database, foodS, foodStock , position;
+var backgroundImg, dogImg;
+
+function preload()
+{
+  dogImg = loadImage("Dog.png");
+  happyDog = loadImage("happydog.png");  
+  //backgroundImg = loadImage("bg.jpg");
+  
+}
+
+
+function setup(){
+    createCanvas(900,500);
+    database= firebase.database();
+    dog = createSprite(450,300,50,50);
+    dog.addImage("dog",dogImg);
+    dog.scale=0.5  ;
+     foodStock =  database.ref('food');
+    foodStock.on("value",readStock)
+}
+
+function draw(){
+    background("turquoise");
+    
+   
+    if(keyWentDown("space")){
+        
+       WriteStock(foodS)
+       dog.addImage("happyDog",happyDog);   
+    }
+    
+
+   
+    
+    drawSprites();
+    textSize(25);
+    fill("black");
+    text("food remaining:"+ foodS,170,80);
+    text("press space to feed the dog!" ,170 , 120 )
+    
+}
+
+function WriteStock(petFOOD){
+    if(petFOOD<=0){
+        petFOOD=0
+    }
+    else{
+        petFOOD=petFOOD-1;
+    }
+    database.ref('/').update({
+        food:petFOOD
+    })
+}
+function readStock(data){
+    foodS = data.val();
+    
+}
+
+
